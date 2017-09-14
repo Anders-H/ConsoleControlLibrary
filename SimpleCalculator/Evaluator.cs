e67f94d027b7a931e7e2ec20e57aee2708c40ea3
Anders-H <anders@winsoft.se>
2017-09-14 21:27:53 +0200

    Started working on parsed structure.

--- SimpleCalculator/Evaluator.cs
+++ SimpleCalculator/Evaluator.cs
@@ -19,7 +19,6 @@ namespace SimpleCalculator
             var tokens = ev.GetTokens(expression);
             if (tokens.Count <= 0)
                 return 0.0;
-            //TODO: Create tree.
             return tokens.Count > 0 ? ev.EvalTokens(tokens) : 0.0;
         }
         private TokenList GetTokens(string expression)
@@ -58,6 +57,25 @@ namespace SimpleCalculator
         }
         private double EvalTokens(TokenList tokens)
         {
+            if (!tokens.Last().IsOperand)
+                throw new SystemException("Must end with operator.");
+            var pass1 = new TokenList();
+            var previous = tokens.First();
+            foreach (var t in tokens)
+            {
+                if (t.IsHighPriorityOperator && previous.IsOperator)
+                    throw new SystemException("Expected operand.");
+                if (t.IsHighPriorityOperator && previous.IsOperand)
+                {
+
+                    var next = tokens.IndexOf(t);
+                }
+                else
+                    pass1.Add(t);
+                previous = t;
+            }
+            foreach (var t in pass1)
+                System.Diagnostics.Debug.WriteLine(t);
             return 0.0;
         }
     }
@@ -68,11 +86,25 @@ namespace SimpleCalculator
         public Token(CharacterClass characterClass, string rawValue)
         {
             CharacterClass = characterClass;
-            RawValue = rawValue;
+            RawValue = (rawValue ?? "").Trim();
         }
+        public string OperatorValue => IsOperator ? RawValue : "";
+        public double OperandValue
+        {
+            get
+            {
+                if (double.TryParse(RawValue, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out double s))
+                    return s;
+                throw new SystemException($"Failed to parse operand: {RawValue}");
+            }
+        }
+        public bool IsHighPriorityOperator => CharacterClass == CharacterClass.Operator && (RawValue == "*" || RawValue == "/");
+        public bool IsLowPriorityOperator => CharacterClass == CharacterClass.Operator && (RawValue == "+" || RawValue == "-");
+        public bool IsOperator => CharacterClass == CharacterClass.Operator;
+        public bool IsOperand => CharacterClass == CharacterClass.Operand;
+        public override string ToString() => $"{CharacterClass}: {RawValue}";
     }
     internal class TokenList : List<Token>
     {
-        
     }
 }
