2abe745b006a784939dedd60758b57e82f863ee4
Anders-H <anders@winsoft.se>
2021-12-12 02:15:34 +0100

    Option to choose draw delegate.

--- ConsoleControlLibrary/Controls/ClientPicture.cs
+++ ConsoleControlLibrary/Controls/ClientPicture.cs
@@ -1,5 +1,4 @@
 ﻿using ConsoleControlLibrary.Controls.BaseTypes;
-using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -7,14 +6,22 @@ namespace ConsoleControlLibrary.Controls
 {
     public class ClientPicture : ControlBase, IControl, IControlFormOperations
     {
-        public delegate void DrawPictureDelegate();
+        public delegate void DrawPictureDelegate(ClientPicture picture, Graphics g);
 
-        private readonly DrawPictureDelegate _drawPictureDelegate;
+        private DrawPictureDelegate _drawPictureDelegate;
 
         public ClientPicture(ConsoleForm parentForm, int x, int y, int width, int height) : base(parentForm, x, y, width, height)
         {
-            if (_drawPictureDelegate != null)
-                _drawPictureDelegate.Invoke();
+        }
+
+        public DrawPictureDelegate DrawPicture
+        {
+            get => _drawPictureDelegate;
+            set
+            {
+                _drawPictureDelegate = value;
+                ParentForm.Invalidate();
+            }
         }
 
         public override void CharacterInput(char c)
@@ -23,7 +30,8 @@ namespace ConsoleControlLibrary.Controls
 
         public override void Draw(Graphics g, IDrawEngine drawEngine)
         {
-            throw new NotImplementedException();
+            if (_drawPictureDelegate != null)
+                _drawPictureDelegate.Invoke(this, g);
         }
 
         public override void KeyPressed(Keys key)
